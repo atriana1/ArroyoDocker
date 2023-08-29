4fe5859ba9b25b8706fe435cbb8dcbd8aa289bef
Andres Triana <andres.triana@qualitycode.com.co>
2023-08-29 18:30:02 -0500

    Final

--- README.md
+++ README.md
@@ -14,6 +14,12 @@ docker run --name arroyo-apache -d -p 80:80 atriana1/arroyo_devops:1.0
 
 docker exec -it arroyo-apache /bin/bash
 
+##  Test Connection between Docker and RDS MySQL
+
+mysql -h arroyomysql.cbtpihhbembs.us-west-2.rds.amazonaws.com -P 3306 -u admin -p Colombia2023.
+
+SELECT * FROM ArroyoDb.Personas;
+
 #  Download Image
 
 docker pull atriana1/arroyo_devops:1.0
--- app/index.php
+++ app/index.php
@@ -8,38 +8,12 @@
 
 <h2>Andres Triana - ArroyoConsulting - Conexión de PHP y MySQL en AWS RDS</h2>
 
-<table border="1">
-<tr>
-    <th>ID</th>
-    <th>Nombre</th>
-    <th>Edad</th>
-</tr>
 <?php
-// Datos de ejemplo
-$personas = array(
-    array("id" => 1, "nombre" => "Juan", "edad" => 25),
-    array("id" => 2, "nombre" => "María", "edad" => 30),
-    array("id" => 3, "nombre" => "Carlos", "edad" => 28),
-);
-
-// Iterar a través de los datos y generar filas de la tabla
-foreach ($personas as $persona) {
-    echo "<tr>";
-    echo "<td>" . $persona["id"] . "</td>";
-    echo "<td>" . $persona["nombre"] . "</td>";
-    echo "<td>" . $persona["edad"] . "</td>";
-    echo "</tr>";
-}
-?>
-</table>
-
-<?php
-/*
 // Configuración de la conexión a la base de datos
-$servername = "localhost";
-$username = "tu_usuario";
-$password = "tu_contraseña";
-$dbname = "nombre_de_la_base_de_datos";
+$servername = "arroyomysql.cbtpihhbembs.us-west-2.rds.amazonaws.com";
+$username = "admin";
+$password = "Colombia2023.";
+$dbname = "ArroyoDb";
 
 // Crear la conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -50,14 +24,14 @@ if ($conn->connect_error) {
 }
 
 // Consulta SQL para obtener los datos de la tabla
-$sql = "SELECT * FROM nombre_de_la_tabla";
+$sql = "SELECT * FROM personas";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // Imprimir los datos en una tabla HTML
-    echo "<table border='1'><tr><th>ID</th><th>Nombre</th></tr>";
+    echo "<table border='1'><tr><th>ID</th><th>Nombre</th><th>Edad</th></tr>";
     while ($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["id"] . "</td><td>" . $row["nombre"] . "</td></tr>";
+        echo "<tr><td>" . $row["id"] . "</td><td>" . $row["nombre"] . "</td><td>" . $row["edad"] . "</td></tr>";
     }
     echo "</table>";
 } else {
@@ -66,7 +40,7 @@ if ($result->num_rows > 0) {
 
 // Cerrar la conexión
 $conn->close();
-*/
+
 ?>
 
 </body>
